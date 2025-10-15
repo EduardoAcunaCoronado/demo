e5a735352f5557904ed1aab07dcabdf245fa4c15
eduardoacunacoronado <edu191191@gmail.com>
2025-10-15 03:00:59 +0200

    Hola mundo.

---
+++ src/main/java/com/ejemplo/demo/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.ejemplo.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hola")
+    public String decirHola() {
+        return "¡Hola desde Spring Boot!";
+    }
+}
