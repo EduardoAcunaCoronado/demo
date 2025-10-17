b72235937c79a2fcdb81cb6d27f954203f4c9ab7
eduardoacunacoronado <edu191191@gmail.com>
2025-10-17 17:35:30 +0200

    Crear archivos de configuración y llamarlos desde clases de configuración.

---
+++ src/main/java/com/ejemplo/tareas/ValuesConfig.java
@@ -0,0 +1,13 @@
+package com.ejemplo.tareas;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:values.properties")
+})
+public class ValuesConfig {
+
+}
--- src/main/java/com/ejemplo/tareas/controllers/PathVariableController.java
+++ src/main/java/com/ejemplo/tareas/controllers/PathVariableController.java
@@ -1,18 +1,29 @@
 package com.ejemplo.tareas.controllers;
 
 import com.ejemplo.tareas.dto.ParamResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.code}")
+    private String code;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
 
     @GetMapping("/baz/{message}")
     public ResponseEntity<ParamResponse> baz(@PathVariable String message) {
@@ -27,6 +38,16 @@ public class PathVariableController {
         Map<String, Object> map = new HashMap<>();
         map.put("product", product);
         map.put("id", id);
+        return map;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> getValues(@Value("${config.message}") String message) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username",  username);
+        map.put("code", code);
+        map.put("message", message);
+        map.put("listOfValues", listOfValues);
         return map;
     }
 
